062b5a785b78deb58377d44b632f1f18c7229ab0
matum <matum@G-Maidana.lysa.com>
2025-10-15 09:38:38 -0300

    primer commit

---
+++ src/main/java/com/mycompany/deso/DESO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deso;
+
+/**
+ *
+ * @author matum
+ */
+public class DESO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
