968278f60d50a4089e8c33cfdae8d528c8e03b00
matum <matum@G-Maidana.lysa.com>
2025-10-16 11:39:43 -0300

    CU02 leemos archivo huespedes

--- src/main/java/dao/HuespedDAO.java
+++ src/main/java/dao/HuespedDAO.java
@@ -1,14 +1,38 @@
 package dao;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author matum
- */
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import models.HuespedDTO;
+
 public class HuespedDAO {
+    private BufferedReader lector;
+    private String linea;
+    private String partes[] = null;
     
+    public void guardar(HuespedDTO huesped){
+        
+    }
+    public void actualizar(HuespedDTO huesped){
+        
+    }
+    public void eliminar(HuespedDTO huesped){
+        
+    }
+    public void buscarHuesped(String nombre, String apellido, String dni,String numero){
+        File archivo = new File("data/ListaHuespedes.csv");
+        
+        try {
+            lector = new BufferedReader(new FileReader(archivo));
+            while ((linea = lector.readLine())!= null){
+                partes = linea.split(",");
+                
+            }
+            
+            
+        }catch(Exepcion e){
+            
+        }
+    }
 }
--- src/main/java/models/HuespedDTO.java
+++ src/main/java/models/HuespedDTO.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author matum
- */
+import java.util.Date;
+
 public class HuespedDTO {
+    public String nombre;
+    public String apellido;
+    public String tipoDeDocumento;
+    public String numeroDocumento;
+    public String posicionFrenteIVA;
+    public int cuit;
+    public int telefono;
+    public Date fecha;    
+    public String email;
+    public String ocupacion;
+    public String nacionalidad;
+    
+    public HuespedDTO(
+        String nombre, 
+        String apellido,
+        String tipoDeDocumento,
+        String numeroDocumento,
+        String posicionFrenteIVA,
+        int cuit,
+        int telefono,
+        Date fecha,
+        String email,
+        String ocupacion,
+        String nacionalidad
+    ){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoDeDocumento = tipoDeDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.posicionFrenteIVA = posicionFrenteIVA;
+        this.cuit = cuit;
+        this.telefono = telefono;
+        this.fecha = fecha;
+        this.email = email;
+        this.ocupacion = ocupacion;
+        this.nacionalidad = nacionalidad;
+    }
     
 }
