964d7098b66957e4dd086a7c22b42d97299fbc43
Joaco <Joaco@JoacoPC>
2025-10-26 15:53:42 -0300

    Agrego Javadoc completo para GitHub Pages

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"dao"},{"l":"exceptions"},{"l":"models"},{"l":"service"},{"l":"ui"},{"l":"util"}];updateSearchResults();
